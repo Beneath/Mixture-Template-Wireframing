f9f6da76ced0dbc82d5f3371d377abf7d798d2f2
Lars Hesselberg <lhe@beneath.dk>
2014-07-29 15:40:48 +0200

    Updated Bootstrap to 3.2, added Windows 7/8 IE fix

--- scripts/master.mix.js
+++ scripts/master.mix.js
@@ -15,3 +15,15 @@
 $(function(){
 	// Write your scripts here
 });
+
+// Windows 7/8 IE fix
+if (navigator.userAgent.match(/Windows NT 6.2; ARM(.+)Touch/)) {
+    var msViewportStyle = document.createElement("style");
+    msViewportStyle.appendChild(
+        document.createTextNode(
+            "@-ms-viewport{width:device-width}"
+        )
+    );
+    document.getElementsByTagName("head")[0].
+        appendChild(msViewportStyle);
+}
